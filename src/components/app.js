404f1324e68a2527d530cb99d68c223a25fc23df
Giamporcaro Jonathan <jonathan.giamporcaro@gmail.com>
2017-03-12 21:07:37 +0100

    books viewer

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import BookList from '../containers/books-list';
+import BookDetail from '../containers/books-detail';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <BookList />
+        <BookDetail />
+      </div>
     );
   }
 }
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Harry Potter', pages: 1010},
+    { title: 'Hunger Games', pages: 42},
+    { title: 'Seigneur des anneaux', pages: 53},
+    { title: 'Star Wars', pages: 275}
+  ];
+}
